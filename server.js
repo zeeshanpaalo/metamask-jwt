b9071d389b75795d2c19ac9da86e0a36439d5af6
zeeshanpaalo <zeeshanpaalo@gmail.com>
2024-02-13 02:03:06 +0500

    created a basic react app

---
+++ server.js
@@ -0,0 +1,1 @@
+// express server for the backend
--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div>
+          <button>Connect Wallet</button>
+        </div>
       </header>
+      <main>
+        <div>
+          <p>
+            List of transactions by user 
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
